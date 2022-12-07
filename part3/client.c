8b7781efba11a1f4b04bf5975a04ba6af68254d3
Guillaume Van Ophem <g.van.ophem@gmail.com>
2022-12-07 13:35:31 +0100

    init part3

---
+++ part3/client.c
@@ -0,0 +1,229 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#include <ctype.h>
+#include <time.h>
+#include <pthread.h>
+#define SA struct sockaddr
+#define ARRAY_TYPE float
+#define BILLION 1000000000
+ 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+ 
+int64_t *receive_times; // en ns
+
+int64_t getts(){
+    struct timespec ts;
+    int err = clock_gettime(CLOCK_MONOTONIC, &ts);
+    if(err == -1){
+        printf("getts failed\n");
+        exit(0);
+    }
+    int64_t tns = ts.tv_sec*1000000000 + ts.tv_nsec;
+    return tns;
+}
+ 
+typedef struct args{
+    struct sockaddr_in server_addr;
+    int max_time;
+    int size;
+    clock_t start;
+    int rate;
+    char* port;
+    int npages;
+    int keysz;
+    int *t;
+}args_t;
+ 
+void print_matrix(ARRAY_TYPE* mat, int dim){
+    for(int i = 0; i < dim; i++){
+        for(int j = 0; j < dim; j++){
+        printf("%4d ", mat[i*dim+j]);
+        }
+        printf("\n");
+    }
+}
+int check(int err, const char *msg){
+    if(err == -1){
+        perror(msg);
+        exit(1);
+    }    
+    return err;
+}
+
+char modulo(int random, int mod){
+    unsigned char c = random%mod;
+    if((int)c < 0){
+        c = (int)c + mod;
+    }
+    return c;
+}
+ 
+void* rcv(void* r) {
+    args_t* arguments = (args_t*)r;
+    struct sockaddr_in servaddr = arguments->server_addr;
+    char* port = arguments->port;
+    int npages = arguments->npages;
+    int keysz = arguments->keysz;
+    int nbytes = arguments->size;
+    int *t = arguments->t;
+    int rate = arguments->rate;
+    int max_time = arguments->max_time;
+ 
+    int ret;
+    int sockfd;
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    check(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)), "connection failed...\n");
+    //Send file id
+    unsigned fileindex = htonl(rand() % npages);
+    ret = send(sockfd, &fileindex, 4, 0);
+    //Send key size
+    //printf("key size : %d\n", keysz);
+    int revkey = htonl(keysz);
+    ret = send(sockfd, &revkey, 4, 0);
+    //Send key
+    ARRAY_TYPE* key = malloc(keysz*keysz * sizeof(ARRAY_TYPE));
+    srand(time(0));
+    for (int i = 0; i < nbytes*nbytes; i++)
+    {
+        int random = rand();
+        key[i] = (ARRAY_TYPE)modulo(random, 100000);
+        //printf("%d ", key[i]);
+    }//printf("\n");
+    ret = send(sockfd, key, sizeof(ARRAY_TYPE) * keysz*keysz, 0);
+    unsigned char error;
+    recv(sockfd, &error, 1, 0);
+    //printf("error code : %d\n", error);
+    unsigned filesz;
+    recv(sockfd, &filesz, 4, 0);
+    filesz = ntohl(filesz);
+    //printf("file size : %d\n", (int)(filesz/sizeof(ARRAY_TYPE)));
+    if (filesz > 0) {
+        ARRAY_TYPE buffer[filesz/sizeof(ARRAY_TYPE)];
+        unsigned tot = filesz;
+        unsigned done = 0;
+        int tread;
+        while( done < tot){
+            if((tread = recv(sockfd, buffer, tot-done, 0)) == -1) return NULL;
+            done+= tread;
+        }
+    }
+    printf("reception done for the %dth time\n", *t);
+    receive_times[((int)(*t))%(max_time*rate)] = getts();
+    close(sockfd);
+}
+
+void print_times(int64_t *times, int l){
+    for(int i = 0; i < l; i++){
+        printf("%lu ", (times[i]));
+    }printf("\n");
+}
+ 
+int main(int argc, char** argv)
+{
+    int opt;
+ 
+    int size = 4;
+    int rate = 1;
+    int max_time = 1;
+ 
+    while ((opt = getopt(argc, argv, ":k:r:t:?")) != -1) {
+        switch (opt) {
+        case 'k':
+            size = atoi(optarg);
+            break;
+        case 'r':
+            rate = atoi(optarg);
+            break;
+        case 't':
+            max_time = atoi(optarg);
+            break;
+        default:
+            break;
+        }
+ 
+    }
+
+    printf("file size : %d\n", size);
+ 
+    int npages = 1000;
+
+    char* str = malloc(strlen(argv[argc-1]));
+    strcpy(str, argv[argc - 1]);
+ 
+    char* token = strtok(str, ":");
+    char* address = malloc(sizeof(char)*strlen(token));
+    memcpy(address, token, strlen(token));
+    token = strtok(NULL, ":");
+    char* port = malloc(strlen(token));
+    memcpy(port, token, strlen(token));
+ 
+ 
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+ 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(address);
+    servaddr.sin_port = htons(atoi(port));
+ 
+ 
+    args_t* arguments = malloc(sizeof(args_t));
+    arguments->server_addr = servaddr;
+    arguments->max_time = max_time;
+    arguments->size = size;
+    arguments->rate = rate;
+    arguments->port = port;
+    arguments->npages = npages;
+    arguments->keysz = size;
+    arguments->t = malloc(sizeof(int));
+    
+    int64_t * sent_times = malloc(sizeof(int64_t) * rate * max_time);
+    if(sent_times == NULL) return -1;
+    receive_times = malloc(sizeof(int64_t) * rate * max_time);
+    int64_t diffrate = 1000000000 / rate;
+    int64_t start = getts();
+    int64_t next = 0;
+    int i = 0;
+    while (getts() - start < (long unsigned)1000000000 * max_time) {
+        next += diffrate;
+        while ((int)(getts() - next) < 0) {
+            usleep((int)(next - getts()) / 1000);
+        }
+        pthread_t thread;
+        arguments->t = realloc(arguments->t, sizeof(int));
+        pthread_mutex_lock(&mutex);
+        int mine = i;
+        sent_times[i%(rate*max_time)] = getts();
+        i++;
+        *(arguments->t) = mine;
+        pthread_mutex_unlock(&mutex);
+        pthread_create( &thread, NULL, rcv, (void*)arguments); 
+        pthread_join(thread, NULL);       
+    }
+
+    printf("---------Sent times[ns]---------\n");
+    print_times(sent_times, rate * max_time);
+    printf("--------Receive times[ns]-------\n");
+    print_times(receive_times, rate*max_time);
+    int64_t *computation_times = malloc(sizeof(int64_t) * rate * max_time);
+    for(int i = 0; i < max_time*rate; i++){
+        computation_times[i] = receive_times[i] - sent_times[i];
+    }
+    printf("------Computation times[ns]-----\n");
+    print_times(computation_times, rate*max_time);
+
+    // free(receive_times);
+    // free(sent_times);
+    // free(computation_times);
+
+    return 0;
+}
---
+++ part3/test_mul.c
@@ -0,0 +1,275 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#include <ctype.h>
+#include <time.h>
+#include <pthread.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <x86intrin.h>
+#define ARRAY_TYPE float
+ 
+ 
+void print_matrix(ARRAY_TYPE* mat, int dim){
+    for(int i = 0; i < dim; i++){
+        for(int j = 0; j < dim; j++){
+        printf("%4f ", mat[i*dim+j]);
+        }
+        printf("\n");
+    }
+}
+ 
+void mat_mul(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+    // ARRAY_TYPE* crypt = malloc(sizeof(ARRAY_TYPE)*nbytes*nbytes);
+    for (int i = 0; i < nr ; i ++) {
+        int vstart = i * keysz;
+        for (int j = 0; j < nr; j++) {
+            int hstart = j * keysz;
+            //Do the sub-matrix multiplication
+            for (int ln = 0; ln < keysz; ln++) {
+                int aline = (vstart + ln) * nbytes + hstart;
+                for (int col = 0; col < keysz; col++) {
+                    int tot = 0;
+                    for (int k = 0; k < keysz; k++) {
+                        int vline = (vstart + k) * nbytes + hstart;
+                        tot += key[ln * keysz + k] * file[vline + col];
+                    }
+                    crypt[aline + col] = tot;
+                }
+            }
+        }
+    }
+}
+ 
+void line_mul(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+ 
+    for(int i = 0; i < nr; i++){
+        int istart = i * keysz;
+        for(int j = 0; j < nr; j++){
+            int jstart = j * keysz;
+            for(int ln = 0; ln < keysz; ln++){
+                for(int col = 0; col < keysz; col++){
+                    ARRAY_TYPE r = key[ln*keysz + col]; //peut etre utilisé file plutôt ici, file[(istart + ln) * nbytes + jstart + col]
+                    for(int k = 0; k < keysz; k++){
+                        //printf("matmul at key[%d,%d] and file[%d,%d]. Writing in crypt[%d,%d]\n", ln, col, istart+col, jstart + k, istart + ln, jstart + k);
+                        crypt[(istart + ln) * nbytes + (jstart + k)] += r * file[(istart + col) * nbytes + (jstart + k)];
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+void line_mul_unrolled(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+    // ARRAY_TYPE* crypt = malloc(sizeof(ARRAY_TYPE)*nbytes*nbytes);
+ 
+    for(int i = 0; i < nr; i++){
+        int istart = i * keysz;
+        for(int j = 0; j < nr; j++){
+            int jstart = j * keysz;
+            for(int ln = 0; ln < keysz; ln++){
+                for(int col = 0; col < keysz; col++){
+                    ARRAY_TYPE r = key[ln*keysz + col]; //peut etre utilisé file plutôt ici, file[(istart + ln) * nbytes + jstart + col]
+                    for(int k = 0; k < keysz; k+=8){
+                        //printf("matmul at key[%d,%d] and file[%d,%d]. Writing in crypt[%d,%d]\n", ln, col, istart+col, jstart + k, istart + ln, jstart + k);
+                        crypt[(istart + ln) * nbytes + (jstart + k)] += r * file[(istart + col) * nbytes + (jstart + k)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+1)] += r * file[(istart + col) * nbytes + (jstart + k+1)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+2)] += r * file[(istart + col) * nbytes + (jstart + k+2)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+3)] += r * file[(istart + col) * nbytes + (jstart + k+3)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+4)] += r * file[(istart + col) * nbytes + (jstart + k+4)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+5)] += r * file[(istart + col) * nbytes + (jstart + k+5)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+6)] += r * file[(istart + col) * nbytes + (jstart + k+6)];
+                        crypt[(istart + ln) * nbytes + (jstart + k+7)] += r * file[(istart + col) * nbytes + (jstart + k+7)];
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+void line_mul_line(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+    for(int i = 0; i < nr; i++){
+        int istart = i * keysz;
+        for(int ln = 0; ln < keysz; ln++){
+            for(int col = 0; col < keysz; col++){
+                ARRAY_TYPE r = key[ln*keysz + col]; //peut etre utilisé file plutôt ici, file[(istart + ln) * nbytes + jstart + col]
+                for(int k = 0; k < nbytes; k++){
+                    // printf("%d\n", crypt[(istart + ln) * nbytes + (k)]);
+                    // printf("matmul at key[%d,%d] and file[%d,%d]. Writing in crypt[%d,%d]\n", ln, col, istart+col, k, istart + ln, k);
+                    crypt[(istart + ln) * nbytes + (k)] += r * file[(istart + col) * nbytes + (k)];
+                }
+            }
+        }
+    }
+}
+ 
+void line_mul_line_unrolled(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+    for(int i = 0; i < nr; i++){
+        int istart = i * keysz;
+        for(int ln = 0; ln < keysz; ln++){
+            for(int col = 0; col < keysz; col++){
+                ARRAY_TYPE r = key[ln*keysz + col]; //peut etre utilisé file plutôt ici, file[(istart + ln) * nbytes + jstart + col]
+                for(int k = 0; k < nbytes; k+=8){
+                    // printf("%d\n", crypt[(istart + ln) * nbytes + (k)]);
+                    // printf("matmul at key[%d,%d] and file[%d,%d]. Writing in crypt[%d,%d]\n", ln, col, istart+col, k, istart + ln, k);
+                    crypt[(istart + ln) * nbytes + (k+0)] += r * file[(istart + col) * nbytes + (k+0)];
+                    crypt[(istart + ln) * nbytes + (k+1)] += r * file[(istart + col) * nbytes + (k+1)];
+                    crypt[(istart + ln) * nbytes + (k+2)] += r * file[(istart + col) * nbytes + (k+2)];
+                    crypt[(istart + ln) * nbytes + (k+3)] += r * file[(istart + col) * nbytes + (k+3)];
+                    crypt[(istart + ln) * nbytes + (k+4)] += r * file[(istart + col) * nbytes + (k+4)];
+                    crypt[(istart + ln) * nbytes + (k+5)] += r * file[(istart + col) * nbytes + (k+5)];
+                    crypt[(istart + ln) * nbytes + (k+6)] += r * file[(istart + col) * nbytes + (k+6)];
+                    crypt[(istart + ln) * nbytes + (k+7)] += r * file[(istart + col) * nbytes + (k+7)];
+ 
+                }
+            }
+        }
+    }
+}
+
+__m256 va, vb, vresult, cryp;
+
+void line_mul_line_unrolled_avx(ARRAY_TYPE* file, ARRAY_TYPE* key, ARRAY_TYPE* crypt, int nbytes, int keysz){
+    int nr = nbytes/keysz;
+    for(int i = 0; i < nr; i++){
+        int istart = i * keysz;
+        for(int ln = 0; ln < keysz; ln++){
+            for(int col = 0; col < keysz; col++){
+                ARRAY_TYPE r = key[ln*keysz + col];
+                for(int k = 0; k < nbytes; k+=8){
+                    va = _mm256_loadu_ps(&file[(istart + col) * nbytes + k]);
+                    vb = _mm256_set_ps(r,r,r,r,r,r,r,r);
+                    cryp = _mm256_loadu_ps(&crypt[(istart + ln) * nbytes + k]);
+                    vresult = _mm256_mul_ps(va, vb);
+                    vresult = _mm256_add_ps(vresult, cryp);
+                    _mm256_store_ps(&crypt[(istart + ln) * nbytes + k], vresult);
+                }
+            }
+        }
+    }
+}
+ 
+ 
+void clear_crypt(ARRAY_TYPE* crypt, int len){
+    for(int i = 0; i<len; i++){
+        crypt[i] = 0;
+    }
+}
+ 
+int main(int argc, char const *argv[])
+{
+    int keysz = 128;
+    int filesz = 1024;
+ 
+    // generate key
+    // ARRAY_TYPE* key = malloc(sizeof(ARRAY_TYPE) * keysz*keysz);
+    ARRAY_TYPE* key = aligned_alloc(64,sizeof(ARRAY_TYPE) * keysz*keysz);
+    for(int i = 0; i < keysz*keysz; i++){
+        key[i] = i % 13;
+    }
+    //print_matrix(key,keysz);
+ 
+    // generate file
+    ARRAY_TYPE* file = aligned_alloc(64,sizeof(ARRAY_TYPE) * filesz * filesz);
+    for(int j = 0; j < filesz * filesz; j++){
+        file[j] = j * 2;
+    }
+    //print_matrix(file,filesz);
+ 
+    // allocate crypt
+    ARRAY_TYPE* crypt = aligned_alloc(64, filesz*filesz*sizeof(ARRAY_TYPE));
+    if(crypt == NULL){
+        printf("failed to alloc\n");
+        exit(0);
+    }
+ 
+    clear_crypt(crypt, filesz*filesz);
+ 
+    // // with mat mul
+    clock_t begin, end;
+    double diff;
+    begin = clock();
+    mat_mul(file, key, crypt, filesz, keysz);  
+    end = clock();
+    diff = (double) (end-begin)/CLOCKS_PER_SEC;
+    printf("Execution time of mat_mul : %f\n", diff);
+    //print_matrix(crypt,filesz);
+ 
+    clear_crypt(crypt, filesz*filesz);
+ 
+    // // with line mul
+    clock_t beg = clock();
+    line_mul(file, key, crypt, filesz, keysz);
+    clock_t finish = clock();
+    double line;
+    line = (double)(finish- beg)/CLOCKS_PER_SEC;
+    printf("Execution time of line_mul : %f\n", line);
+    // //print_matrix(crypt,filesz);
+ 
+    // clear_crypt(crypt, filesz*filesz);
+ 
+    // with line mul2
+    clock_t beg2 = clock();
+    line_mul_unrolled(file, key, crypt, filesz, keysz);
+    clock_t finish2 = clock();
+    double line2;
+    line2 = (double)(finish2- beg2)/CLOCKS_PER_SEC;
+    printf("Execution time of line_mul_unrolled : %f\n", line2);
+    //print_matrix(crypt,filesz);
+ 
+    clear_crypt(crypt, filesz*filesz);
+ 
+    // with line mul line
+    clock_t beg3 = clock();
+    line_mul_line(file, key, crypt, filesz, keysz);
+    clock_t finish3 = clock();
+    double line3;
+    line3 = (double)(finish3- beg3)/CLOCKS_PER_SEC;
+    printf("Execution time of line_mul_line : %f\n", line3);
+    //print_matrix(crypt,filesz);
+ 
+    clear_crypt(crypt,filesz*filesz);
+ 
+    //with line_mul_line_unrolled
+    clock_t beg4 = clock();
+    line_mul_line_unrolled(file, key, crypt, filesz, keysz);
+    clock_t finish4 = clock();
+    double line4;
+    line4 = (double)(finish4- beg4)/CLOCKS_PER_SEC;
+    printf("Execution time of line_mul_line_unrolled : %f\n", line4);
+    //print_matrix(crypt,filesz);
+
+    clear_crypt(crypt, filesz*filesz);
+
+    //with line_mul_line_unrolled_avx
+    clock_t beg5 = clock();
+    line_mul_line_unrolled_avx(file, key, crypt, filesz, keysz);
+    clock_t finish5 = clock();
+    double line5 = (double)(finish5-beg5)/CLOCKS_PER_SEC;
+    printf("Execution time of line_mul_line_unrolled_avx : %f\n", line5);
+    //print_matrix(crypt, filesz);
+
+ 
+ 
+    free(crypt);
+ 
+ 
+    return 0;
+}
